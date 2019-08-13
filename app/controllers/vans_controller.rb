3dc1c33f6a55976daa30b644683f63b15dfc1ab2
TCHALLA Nina Essowaza <nina.tchalla@gmail.com>
2019-08-13 19:01:20 +0200

    Finished van list

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -1,5 +1,13 @@
 class VansController < ApplicationController
   def index
+    if params[:search]
+      query = params[:search]
+      begin_date = Date.new(query["begin_date(1i)"].to_i, query["begin_date(2i)"].to_i, query["begin_date(3i)"].to_i)
+      end_date = Date.new(query["end_date(1i)"].to_i, query["end_date(2i)"].to_i, query["end_date(3i)"].to_i)
+      @vans = Van.all
+    else
+      @vans = Van.all
+    end
   end
 
   def show

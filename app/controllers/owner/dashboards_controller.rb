3370cb45d2c8fdca469226a34d01b1c964a38f0a
TCHALLA Nina Essowaza <nina.tchalla@gmail.com>
2019-08-14 16:47:09 +0200

    Fixed sign-up, owner dashboard rents

--- app/controllers/owner/dashboards_controller.rb
+++ app/controllers/owner/dashboards_controller.rb
@@ -1,8 +1,6 @@
 class Owner::DashboardsController < ApplicationController
   def show
     @vans = current_user.vans
-    @rents = Rent.select { |rent| rent.van.owner = current_user }
+    @rents = Rent.select { |rent| rent.van.owner == current_user }
   end
-
-
 end
--- app/models/van.rb
+++ app/models/van.rb
@@ -1,5 +1,6 @@
 class Van < ApplicationRecord
   mount_uploader :picture, PhotoUploader
+
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   has_many :rents
 

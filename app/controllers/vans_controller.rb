ef9210a404dc5d59cf9aa8ab9ad9400a4ff77952
TCHALLA Nina Essowaza <nina.tchalla@gmail.com>
2019-08-14 14:39:00 +0200

    Started van show

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -4,8 +4,9 @@ class VansController < ApplicationController
   def index
     if params[:search]
       q = params[:search]
-      begin_date = !q[:begin_date].empty? && Date.valid_date?(q[:begin_date]) ? Date.parse(q[:begin_date]) : Date.today
-      end_date = !q[:end_date].empty? && Date.valid_date?(q[:end_date]) ? Date.parse(q[:end_date]) : Date.today
+      # TODO Stronger verification her
+      begin_date = !q[:begin_date].empty? ? Date.parse(q[:begin_date]) : Date.today
+      end_date = !q[:end_date].empty? ? Date.parse(q[:end_date]) : Date.today
 
       @vans = Van.select { |van| van.available?(begin_date: begin_date, end_date: end_date) }
     else
@@ -14,5 +15,7 @@ class VansController < ApplicationController
   end
 
   def show
+    @van = Van.find(params[:id])
+    @rent = Rent.new
   end
 end
